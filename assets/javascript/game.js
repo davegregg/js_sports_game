29e28ee9e3bb0f3e9428718591ed3d4f529efb83
Elise Cossairt <elise_cossairt@hotmail.comgit config --global user.name Eliserunclear--globalgit config user.email elise_cossairt@hotmail.com>
2020-05-04 16:11:05 -0500

    added sounds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,14 @@
+const audioGoal = new Audio("http://dight310.byu.edu/media/audio/FreeLoops.com/1/1/Basketball%20Swish-4011-Free-Loops.com.mp3");
+
+const audioBuzzer = new Audio("http://thebeautybrains.com/wp-content/uploads/podcast/soundfx/shotclockhorn.wav");
+
 const oneShots = document.querySelector("#teamone-shoot-button");
 oneShots.addEventListener("click", function Random() {
     console.log ("Team one shot taken");
     document.getElementById("teamone-numshots").innerText++;
     if (Math.floor(Math.random() * 2 ) > 0 ) {
         document.getElementById("teamone-numgoals").innerHTML++;
+        audioGoal.play();
     } 
 })
 
@@ -12,16 +17,20 @@ twoShots.addEventListener("click", function Random() {
     console.log ("Team two shot taken");
     document.getElementById("teamtwo-numshots").innerText++;
     if (Math.floor(Math.random() * 2 ) > 0 ) {
-    document.getElementById("teamtwo-numgoals").innerText++;
+        document.getElementById("teamtwo-numgoals").innerText++;
+        audioGoal.play();
     }
 })
 
 const resets = document.querySelector("#reset-button");
 resets.addEventListener("click", function () {
     console.log ("Game Reset");
+    audioBuzzer.play();
     document.getElementById("num-resets").innerText++;
     document.getElementById("teamone-numshots").innerText = 0;
     document.getElementById("teamone-numgoals").innerText = 0;
     document.getElementById("teamtwo-numshots").innerText = 0;
     document.getElementById("teamtwo-numgoals").innerText = 0;
 })
+
+
