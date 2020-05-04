104ed62baed7fe0862c5f24d4efbd8530623c1ef
Elise Cossairt <elise_cossairt@hotmail.comgit config --global user.name Eliserunclear--globalgit config user.email elise_cossairt@hotmail.com>
2020-05-04 09:54:31 -0500

    Done!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,25 @@
-let oneNumShots = 1
 const oneShots = document.querySelector("#teamone-shoot-button");
 oneShots.addEventListener("click", function Random() {
     console.log ("Team one shot taken");
-    document.getElementById("teamone-numshots").innerText +++ oneNumShots;
-    document.getElementById("teamone-numgoals").innerText -= Math.floor(Math.random() * 2);
+    document.getElementById("teamone-numshots").innerText++;
+    if (Math.floor(Math.random() * 2 ) > 0 ) {
+        document.getElementById("teamone-numgoals").innerHTML++;
+    } 
 })
 
-
-
-let twoNumShots = 1
 const twoShots = document.querySelector("#teamtwo-shoot-button");
 twoShots.addEventListener("click", function Random() {
     console.log ("Team two shot taken");
-    document.getElementById("teamtwo-numshots").innerText +++ twoNumShots;
-    document.getElementById("teamtwo-numgoals").innerText -+ Math.floor(Math.random() * 2);
+    document.getElementById("teamtwo-numshots").innerText++;
+    if (Math.floor(Math.random() * 2 ) > 0 ) {
+    document.getElementById("teamtwo-numgoals").innerText++;
+    }
 })
 
-let numResets = 1
 const resets = document.querySelector("#reset-button");
 resets.addEventListener("click", function () {
     console.log ("Game Reset");
-    document.getElementById("num-resets").innerText +++ numResets;
+    document.getElementById("num-resets").innerText++;
     document.getElementById("teamone-numshots").innerText = 0;
     document.getElementById("teamone-numgoals").innerText = 0;
     document.getElementById("teamtwo-numshots").innerText = 0;
